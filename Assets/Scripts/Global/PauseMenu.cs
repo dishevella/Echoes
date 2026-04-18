1072c4a18bdb5bab906ce4a0c90e5be5faf3393f
NotYoursLOL <aokisama1036@gmail.com>
2026-04-18 18:26:03 +0800

    pause menu

---
+++ Assets/Scripts/Global/PauseMenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitLevel()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        //back to Level Scene
+    }
+
+    public void Restart()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        //Restart
+    }
+}
